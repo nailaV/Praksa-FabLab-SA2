21c18c99a983a80a52b705369922f87c76b58b01
Adis Šipković <94229595+adis-01@users.noreply.github.com>
2023-08-04 16:43:48 +0200

    insert logic added

--- js/addTeamMember.js
+++ js/addTeamMember.js
@@ -17,115 +17,115 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-function ResetValues(name, surname,mail,linkedInLink,quote, fileN, slicica){
-    name.value="";
-    surname.value="";
+function ResetValues(name, surname, mail, linkedInLink, quote, fileN, slicica) {
+    name.value = "";
+    surname.value = "";
     //title.value="";
-    linkedInLink.value="";
-    mail.value="";
-    quote.value="";
-    fileN.value="";
-    slicica.style.display="none";
+    linkedInLink.value = "";
+    mail.value = "";
+    quote.value = "";
+    fileN.value = "";
+    slicica.style.display = "none";
 }
 
 
-function Prijava(){
-    const klik=document.getElementById("formaSignIn");
-    klik.style.display="block";
-    document.getElementById("overlay").style.display="block";
+function Prijava() {
+    const klik = document.getElementById("formaSignIn");
+    klik.style.display = "block";
+    document.getElementById("overlay").style.display = "block";
 }
 
 
-function SignInn(){
+function SignInn() {
     const user = document.querySelector("#username1");
     const pass = document.querySelector("#password1");
     firebase.auth().signInWithEmailAndPassword(user.value, pass.value)
-    .then((userCredential) => {
-        console.log('prijavljen');
-        //document.getElementById("UserInfo").style.display="block";
-        Zatvori();
-    })
-    .catch((error) => { 
-        alert(error.message);
-    });
+        .then((userCredential) => {
+            console.log('prijavljen');
+            //document.getElementById("UserInfo").style.display="block";
+            Zatvori();
+        })
+        .catch((error) => {
+            alert(error.message);
+        });
 }
 
 
-    
 
-function Zatvori(){
-    const klik=document.getElementById("formaSignIn");
-    klik.style.display="none";
-    document.getElementById("overlay").style.display="none";
+
+function Zatvori() {
+    const klik = document.getElementById("formaSignIn");
+    klik.style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
 
-function Odjava(){
+function Odjava() {
     firebase.auth().signOut().then(() => {
         console.log('odjavljen');
         //document.getElementById("UserInfo").style.display="none";
-        
+
     }).catch((error) => {
         alert(error.message);
     });
 }
 
 
-function OpenModal(){
-    document.getElementById("forma").style.display="block";
-    document.getElementById("overlay").style.display="block";
+function OpenModal() {
+    document.getElementById("forma").style.display = "block";
+    document.getElementById("overlay").style.display = "block";
 }
 
-function CloseModal(){
+function CloseModal() {
     const name = document.getElementById("firstName");
-     const surname = document.getElementById("lastName");
+    const surname = document.getElementById("lastName");
     // const title = document.getElementById("title");
     const mail = document.getElementById("email");
     const linkedInLink = document.getElementById("hyperlink-input");
     const quote = document.getElementById("qoute");
-    const fileN=document.getElementById("fileUpload");
-    const slicica=document.querySelector(".image ");
-    ResetValues(name,surname,mail,linkedInLink,quote,fileN, slicica);
-    document.getElementById("forma").style.display="none";
-    document.getElementById("overlay").style.display="none";
+    const fileN = document.getElementById("fileUpload");
+    const slicica = document.querySelector(".image ");
+    ResetValues(name, surname, mail, linkedInLink, quote, fileN, slicica);
+    document.getElementById("forma").style.display = "none";
+    document.getElementById("overlay").style.display = "none";
 }
 
-function ImageUploadValidation(){
+function ImageUploadValidation() {
     const imageUpload = document.getElementById("fileUpload").value;
-    if(!imageUpload){
+    if (!imageUpload) {
         alert("You must upload an image");
         return false;
     }
     else {
         const fileExtension = imageUpload.split(".").pop();
-        if (fileExtension !== "jpg" && fileExtension !== "png" ) {
-          alert("You can only upload JPG or PNG images.");
-          return false;
+        if (fileExtension !== "jpg" && fileExtension !== "png") {
+            alert("You can only upload JPG or PNG images.");
+            return false;
         }
-      }
-      return true;
+    }
+    return true;
 }
 
-function TextInputValidation(){
-     const name = document.getElementById("firstName").value;
-     const surname = document.getElementById("lastName").value;
-     //const title = document.getElementById("title").value;
+function TextInputValidation() {
+    const name = document.getElementById("firstName").value;
+    const surname = document.getElementById("lastName").value;
+    const title = document.getElementById("title").value;
     const mail = document.getElementById("email").value;
     const linkedInLink = document.getElementById("hyperlink-input").value;
     const quote = document.getElementById("qoute").value;
     const imageUpload = document.getElementById("fileUpload").value;
-   
 
-     
-    if(isNullOrEmpty(name) || isNullOrEmpty(surname) || isNullOrEmpty(title) ||imageUpload==undefined|| isNullOrEmpty(mail) || isNullOrEmpty(linkedInLink) || isNullOrEmpty(quote)){
+
+
+    if (isNullOrEmpty(name) || isNullOrEmpty(surname) || isNullOrEmpty(title) || isNullOrEmpty(title) || imageUpload == undefined || isNullOrEmpty(mail) || isNullOrEmpty(linkedInLink) || isNullOrEmpty(quote)) {
         alert("All fields are required.");
         return false;
     }
 
-    if(name.length > 25 || containsNumber(name)){
+    if (name.length > 25 || containsNumber(name)) {
         alert("Name must be valid, maximum of 25 characters, only letters.");
         return false;
     }
-    if(surname.length > 30 || containsNumber(surname)){
+    if (surname.length > 30 || containsNumber(surname)) {
         alert("Surname must be valid, maximum of 30 characters, only letters.");
         return false;
     }
@@ -134,81 +134,124 @@ function TextInputValidation(){
     //     return false;
     // }
 
-    if(!isValidEmail(mail)){
+    if (!isValidEmail(mail)) {
         alert("Email must be valid - for example name.surname@mailprovider.com.");
         return false;
     }
 
-    if(quote.length > 50 ){
+    if (quote.length > 50) {
         alert("Quote must be valid, maximum of 50 characters.");
         return false;
     }
 
-    if(!isValidHyperlink(linkedInLink)){
+    if (!isValidHyperlink(linkedInLink)) {
         alert("You must enter valid hyperlink - for example https://www.linkedin.com/in/your-profile-credentials");
         return false;
     }
 
-    if(!ImageUploadValidation())
-    {
+    if (!ImageUploadValidation()) {
         return false;
     }
     return true;
 }
-function isValidEmail(email){
+function isValidEmail(email) {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
 }
-function containsNumber(tekst){
+function containsNumber(tekst) {
     const regex = /\d/;
     return regex.test(tekst);
 }
-function isValidHyperlink(url) { 
-        const linkedinRegex = /^(https?:\/\/)?(www\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+\/?$/;
-        return linkedinRegex.test(url);
+function isValidHyperlink(url) {
+    const linkedinRegex = /^(https?:\/\/)?(www\.)?linkedin\.com\/in\/[a-zA-Z0-9_-]+\/?$/;
+    return linkedinRegex.test(url);
 }
 
-firebase.auth().onAuthStateChanged((user)=>{
-    if(user){
-        document.getElementById("UserInfo").style.display="block";
-        document.getElementById("prijavaDugme").style.display="none";
-        document.getElementById("odjavaDugme").style.display="block";
-        document.getElementById("UserInfoOdjava").style.display="none";
-        document.getElementById("dugmic").style.display="block";
+firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+        document.getElementById("UserInfo").style.display = "block";
+        document.getElementById("prijavaDugme").style.display = "none";
+        document.getElementById("odjavaDugme").style.display = "block";
+        document.getElementById("UserInfoOdjava").style.display = "none";
+        document.getElementById("dugmic").style.display = "block";
     }
-    else{
-        document.getElementById("UserInfo").style.display="none";
-        document.getElementById("odjavaDugme").style.display="none";
-        document.getElementById("prijavaDugme").style.display="block";
-        document.getElementById("UserInfoOdjava").style.display="block";
-        document.getElementById("dugmic").style.display="none";
+    else {
+        document.getElementById("UserInfo").style.display = "none";
+        document.getElementById("odjavaDugme").style.display = "none";
+        document.getElementById("prijavaDugme").style.display = "block";
+        document.getElementById("UserInfoOdjava").style.display = "block";
+        document.getElementById("dugmic").style.display = "none";
     }
 });
 
-function isNullOrEmpty(inputText){
+function isNullOrEmpty(inputText) {
     return inputText === null || inputText === undefined || inputText.trim().length === 0;
 }
 
-//imagePreview
-const slika=document.querySelector('#fileUpload');
-const divSlike=document.querySelector('.image');
 
-slika.addEventListener('change', (event)=> {
+
+//imagePreview
+const slika = document.querySelector('#fileUpload');
+const divSlike = document.querySelector('.image');
+
+slika.addEventListener('change', (event) => {
     const file = event.target.files[0];
-  
+
     if (file) {
-      const reader = new FileReader();
-  
-      reader.onload = function() {
-        const imagePreview = document.querySelector('.image');
-        imagePreview.style.display = 'block';
-        imagePreview.style.backgroundImage = `url(${reader.result})`;
-      }
-  
-      reader.readAsDataURL(file);
+        const reader = new FileReader();
+
+        reader.onload = function () {
+            const imagePreview = document.querySelector('.image');
+            imagePreview.style.display = 'block';
+            imagePreview.style.backgroundImage = `url(${reader.result})`;
+        }
+
+        reader.readAsDataURL(file);
     } else {
-      const imagePreview = document.querySelector('.image');
-      imagePreview.style.display = 'none';
-      imagePreview.style.backgroundImage = '';
-   }
- });
+        const imagePreview = document.querySelector('.image');
+        imagePreview.style.display = 'none';
+        imagePreview.style.backgroundImage = '';
+    }
+});
+
+function AddTeamMember() {
+    
+    if (!TextInputValidation()) {
+        console.log('nije uredu');
+        return;
+    }
+    else {
+        const nameInput = document.getElementById("firstName").value;
+        const surnameInput = document.getElementById("lastName").value;
+        const titleInput = document.getElementById("title").value;
+        const mailInput = document.getElementById("email").value;
+        const linkedInLinkInput = document.getElementById("hyperlink-input").value;
+        const quoteInput = document.getElementById("qoute").value;
+        const imageInput = document.getElementById("fileUpload").files[0];
+        const divElement = document.createElement('divZaMembera');
+        divElement.innerHTML = `
+    <div class="flexInnerTeam lightBg">
+      <header>
+        <h3 class="NameTitle">${nameInput} ${surnameInput} <br> <small>${titleInput}</small></h3>
+      </header>
+      <div class="flexDivInner">
+      <img src="${imageInput ? URL.createObjectURL(imageInput) : ''}" alt="Load" class="w3-left w3-circle w3-margin-right imgRound">
+        <p>${quoteInput}</p>
+    </div>
+    <p class="mailAlignment"> ${mailInput}</p>
+      <a href="${linkedInLinkInput}" target="_blank">
+        <div class="w3-button w3-block liBg"><img src="../../slike/6.png" alt="">
+        </div>
+      </a>
+    </div>
+    `;
+
+        const getDivElement = document.querySelector(".flexTeam");
+        getDivElement.appendChild(divElement);
+
+
+
+        CloseModal();
+    }
+
+}
